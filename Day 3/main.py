aeba6675f08252fc26d22ae4e16ba0526c2b60de
syahirahsabri <78020060+syahirahsabri@users.noreply.github.com>
2025-03-14 09:46:35 +0800

    Update main.py

--- Day 3/main.py
+++ Day 3/main.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(page_title="test", page_icon=":shark:",layout="wide")
 from PIL import Image
-im = Image.open("../Day 3/shrdc_logo.png")
+im = Image.open("./Day 3/shrdc_logo.png")
 st.image(im, width=300)
 st.markdown("Hello World!")
 
